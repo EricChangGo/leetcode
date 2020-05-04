a3ccff622b5953c78b73fe8ced074f856caa8677
Blacky <ericblackking@gmail.com>
2020-05-04 20:08:41 +0800

    binary insert & linear insert

--- 35.search-insert-position.go
+++ 35.search-insert-position.go
@@ -5,27 +5,21 @@
  */
 
 // @lc code=start
-func searchInsert(nums []int, target int) int {
-	// head, tail 
+func binaryInsert(nums []int, len_nums int, target int) int {
+	// head, tail
 	start := 0
-	end := len(nums)-1
+	end := len_nums-1
 	insert_idx := -1
-	if target < nums[0] {
-			return 0
-	}
-	if target > nums[end] {
-			return end+1
-	}
-	
+	check := 0
 	for insert_idx < 0 {
-			check := (start+end)/2
+			check = (start+end)/2
 			
 			if check == start {
 					// find insert point, insert between the start end
-					insert_idx = end
-					if target <= nums[start] {
+					insert_idx = start+1
+					if target == nums[start] {
 							 // target value equals to start, insert in front of start
-							 insert_idx = start
+							 insert_idx -= 1
 					}
 			}
 			
@@ -36,6 +30,30 @@ func searchInsert(nums []int, target int) int {
 			}
 	}
 	return insert_idx
+}
+
+func linearInsert(nums []int, len_nums int, target int) int {
+	for i:=0; i<len_nums-1; i++ {
+			if nums[i] < target && target <= nums[i+1] {
+					return i+1
+			} 
+	}
+	return 0
+}
+
+func searchInsert(nums []int, target int) int {
+	if target < nums[0] {
+			return 0
+	}
+	len_nums := len(nums)
+	if target > nums[len_nums-1] {
+			return len_nums
+	}
+	
+	if(len_nums < 1000) {
+			return linearInsert(nums, len_nums, target)
+	}
+	return binaryInsert(nums, len_nums, target) 
 }
 // @lc code=end
 
