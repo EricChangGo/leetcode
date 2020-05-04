bd7550c054aa5d556c35ef60ef554ee7b3c2fb76
Blacky <ericblackking@gmail.com>
2020-05-04 12:16:56 +0800

    move cpp into file

--- cplusplus/122.best-time-to-buy-and-sell-stock-ii.cpp
+++ cplusplus/122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -9,54 +9,20 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {  
       int t_profit = 0;
-      int hold_stock = 0;
-      vector<pair<int, int>> profits_idx;
-      profits_idx.push_back(pair<int, int> (0, 0));
-      
+      int buy_idx = 0;
+      int sale_idx = 0;
+      /**
+        1. better min -> change holding stock, (buy, sale) = (i,i)
+        2. new stock (old buy value <value < old sale value) -> sale the old one, (buy, sale) = (i,i)
+        3. larger sale day -> recount the profit
+      */
       for(int i=1; i<prices.size(); i++) {
-        // update profit
-        int target_value = prices[i];
-        int max_add_value = 0;
-        int exchange_idx = -1;
-
-        /**
-          Key: you must sell the stock before you buy again
-          Try to arrange to sell this i day.
-        */
-        for(int j=0; j<profits_idx.size(); j++) {
-          
-          // check overlap
-          if(profits_idx[hold_stock].second > profits_idx[j].second) {
-            exchange_idx == -2;
-            continue;
-          }
-
-          if(target_value > prices[profits_idx[j].first]) {
-            int add_value = target_value - prices[profits_idx[j].second];
-            if(add_value > max_add_value) {
-              max_add_value = add_value;
-              exchange_idx = j;
-            }
-          }
-        }
-        
-        if(exchange_idx == -2) {
-          profits_idx.clear();
-          continue;
-        }
-
-        if(exchange_idx == -1) {
-          profits_idx.clear();
-          profits_idx.push_back(pair<int, int> (i, i));
-        }
-        else {
-          // sale in day i, which buy from day exchange_idx
-          hold_stock = exchange_idx;
-          t_profit += (prices[i] - prices[profits_idx[exchange_idx].second]);
-          profits_idx[exchange_idx].second = i;
-        }
+        if(prices[i] > prices[sale_idx]) 
+          t_profit += (prices[i] - prices[sale_idx]);
+        else 
+          buy_idx = i;
+        sale_idx = i;
       }
-      
       return t_profit;
     }
 };
